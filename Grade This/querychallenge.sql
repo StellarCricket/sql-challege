b18e7c1310b4475e75a561956455a0efc3bec96b
Lyndsi <lyndsi.wolf@gmail.com>
2020-02-03 21:01:59 -0500

    Initial HW submission

---
+++ Grade This/querychallenge.sql
@@ -0,0 +1,91 @@
+-- 1.List the following details of each employee: employee
+-- number, last name, first name, gender, and salary.
+
+SELECT e.emp_no, e.first_name, e.last_name, e.gender,  s.salary
+FROM salaries AS s
+
+INNER JOIN employees AS e ON
+e.emp_no=s.emp_no;
+
+
+-- 2. List employees who were hired in 1986.
+
+SELECT e.hire_date, e.first_name, e.last_name 
+FROM employees AS e
+WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01'
+
+
+-- 3.List the manager of each department with the following information:
+-- department number, department name, the managers employee number, 
+-- last name, first name, and start and end employment dates.
+
+SELECT d.dept_no, d.dept_name, m.emp_no, m.from_date, m.to_date,
+e.first_name, e.last_name
+FROM departments AS d
+INNER JOIN dept_manager AS m ON
+d.dept_no=m.dept_no
+INNER JOIN employees AS e ON
+e.emp_no=m.emp_no;
+
+ 
+-- 4.List the department of each employee with the following
+-- information: employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e. first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS dm ON
+dm.emp_no=e.emp_no
+INNER JOIN departments AS d ON
+d.dept_no=dm.dept_no
+ORDER BY "emp_no" DESC;
+
+
+5.-- List all employees whose first name is 
+-- "Hercules" and last names begin with "B."
+
+SELECT first_name, last_name 
+FROM employees
+WHERE first_name='Hercules'
+AND last_name LIKE 'B%';
+
+
+6.
+-- List all employees in the Sales department, 
+-- including their employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e. first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS dm ON
+dm.emp_no=e.emp_no
+INNER JOIN departments AS d ON
+d.dept_no=dm.dept_no
+WHERE d.dept_name = 'Sales' OR
+d.dept_name = 'Development'
+ORDER BY "emp_no" DESC;
+
+
+7.-- List all employees in the Sales department, 
+-- including their employee number, last name, first name, and department name.
+
+
+SELECT e.emp_no, e.last_name, e. first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS dm ON
+dm.emp_no=e.emp_no
+INNER JOIN departments AS d ON
+d.dept_no=dm.dept_no
+WHERE d.dept_name = 'Sales' OR
+d.dept_name = 'Development'
+ORDER BY "emp_no" DESC;
+
+8.-- In descending order, list the frequency count of employee
+-- last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name)AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY "frequency" DESC;
+
+
+
+
---
+++ Grade This/tablecreation.sql
@@ -0,0 +1,71 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees; 
+DROP TABLE IF EXISTS salaries; 
+DROP TABLE IF EXISTS titles; 
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" VARCHAR   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "emp_no" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "dept_emp" ("dept_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "dept_manager" ("dept_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "titles" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "salaries" ("emp_no");
+
